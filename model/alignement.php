eb69c2a0ff727eb101b523697088916ee1ee211d
Romain <romainfatali30@gmail.com>
2023-02-07 13:31:51 +0100

    Organisation des différentes classes du modèle

--- model/alignement.php
+++ model/alignement.php
@@ -4,11 +4,11 @@ class Alignement extends Objet {
     // attributs de classe
 	protected static $objet = "Alignement";
 
-    protected $alignement;
+    protected $id_alignement;
+    protected $id_utilisateur;
 
-  
-    
+    public function afficher(){
+        echo Alignement::$objet . " n°" . $this->id_alignement . " est lié à l'Utilisateur n°" . $this->id_utilisateur;
+    }
 }
-
-
 ?>
