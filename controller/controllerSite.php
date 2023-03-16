7447f8630dddb29e41327366b8738fbc808a438b
Clément <75119441+switch9278@users.noreply.github.com>
2023-03-16 09:39:12 +0100

    Add files via upload
    - Ajout des fonctions effacées du model

--- controller/controllerSite.php
+++ controller/controllerSite.php
@@ -1,6 +1,11 @@
 <?php
 
+require_once("../model/objet.php");
+require_once("../model/utilisateur.php");
+
 class controllerSite{
+
+protected $utilisateur = Utilisateur::getObjetById($_SESSION["id"]);
 
 public static function homePage(){
     $title = "Accueil";
@@ -13,5 +18,67 @@ public static function error404(){
     require_once("view/header.php");
     require_once("view/footer.php");
 }
+
+public static function proposerAlignement(){
+    if($utilisateur->estConnecte()){
+        Alignement::creerAlignement($utilisateur->id_utilisateur, $alignement, $reponses);
+    }    
+}
+
+public static function validerAlignement(){
+    if ($utilisateur->isChef){
+        Alignement::validerAlignement($alignement, $partie);
+    }
+}
+
+public static function voirLesAlignementsProposes()
+{
+    if($utilisateur->estConnecte()){
+        Alignement::getAllObjets();
+    }
+}
+
+public function ajouterExpression()
+{
+    if($utilisateur->isAdmin){
+        Expression::ajouterExpression($textelangue, $litteraltrad, $theme, $pays, $langue);
+    }
+}
+
+public function modifierExpression()
+{
+    if($utilisateur->isAdmin){
+        Expression::modifierExpression($expression, $textelangue, $litteraltrad, $theme, $pays, $langue);
+    }
+}
+
+public function supprimerExpression()
+{
+    if($utilisateur->isAdmin){
+        Expression::supprimerExpression($expression);
+    }
+}
+
+public function ajouterLangue()
+{
+    if ($utilisateur->isAdmin){
+        Langue::ajouterLangue($langue, $code, $groupelangue);
+    }
+}
+
+public function modifierLangue()
+{
+    if ($utilisateur->isAdmin){
+        Langue::modifierLangue($idlangue, $langue, $code, $groupelangue);
+    }
+}
+
+public function supprimerLangue()
+{
+    if ($utilisateur->isAdmin){
+        Langue::supprimerLangue($langue);
+    }
+}
+
 }
 ?>
