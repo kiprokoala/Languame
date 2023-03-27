245758ec7b2899f5fa962610b4d64fc744b8a98d
Romain <82060667+kiprokoala@users.noreply.github.com>
2023-03-27 11:57:06 +0200

    Correctifs + class pays

--- model/pays.php
+++ model/pays.php
@@ -14,8 +14,15 @@ class Pays extends Objet {
         Pays::$objet . " n°" . $this->id_pays . " a pour nom " . $this->nomPays . ". Son raccoursi est " . $this->raccourciPays . ". Il a pour latitude " . $this->latitudeCapitalePays . " et pour longitude " . $this->longitudeCapitalePays;
     }
 
-    public function getExpressionsByTheme(){
-
+    public function getExpressionsByPays(){
+        $requete = "SELECT id_expression FROM Expression WHERE id_pays = ".$this->get("id_pays").";";
+        try {
+            $resultat = Connexion::pdo()->query($requete);
+            $resultat->setFetchmode(PDO::FETCH_NUM);
+            return $resultat->fetchAll();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
 }
 
