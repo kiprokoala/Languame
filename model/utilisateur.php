cd287853836bd358333d654679ffd21d844a9582
Romain <82060667+kiprokoala@users.noreply.github.com>
2023-03-27 11:32:43 +0200

    Revert "Merge branch 'alignement-CG'"
    This reverts commit f0ee046375e58656b2f68e44f9d1e5f9d6e61fb4, reversing
    changes made to 9ebb36dcad1f106686212d12619e8965db468c86.

--- model/utilisateur.php
+++ model/utilisateur.php
@@ -22,22 +22,29 @@ class Utilisateur extends Objet
         Utilisateur::$objet . " n°" . $this->id_utilisateur . " a pour login " . $this->login . ", pour prénom " . $this->prenom . " et pour nom " . $this->nom . ". Son isChef est à " . $this->isChef . " et son isAdmin est à " . $this->isAdmin;
     }
 
+    /*
+     *
+     * Fonctions d'utilisateur
+     *
+    */
 
     public function creerUtilisateur($login, $mdp, $prenom, $nom, $isAdmin)
     {
         self::addObjet(get_defined_vars());
     }
 
+    // Yoplait
+
     public function modifierUtilisateur($id_utilisateur, $login, $mdp, $prenom, $nom, $isAdmin)
     {
-        if($id_utilisateur == self::estConnecte() || $this->isAdmin){
+        if($id_utilisateur == estConnecte() || $this->isAdmin){
             self::updateObjet(get_defined_vars());
         }
     }
 
     public function supprimerUtilisateur($id_utilisateur)
     {
-        if($id_utilisateur == self::estConnecte() || $this->isAdmin){
+        if($id_utilisateur == estConnecte() || $this->isAdmin){
             self::deleteObjetById($id_utilisateur);
         }
     }
@@ -65,7 +72,98 @@ class Utilisateur extends Objet
     }
 
     public function estConnecte(){
-        $_SESSION["id"] ? return true : return false;
+        return $_SESSION["id"];
     }
+
+    public function proposerAlignement($alignement, $reponses)
+    {
+        //Avoir un rendu visuel à la fin de la création de l'alignement
+        Alignement::creerAlignement($this->id_utilisateur, $alignement, $reponses);
+    }
+
+    public function validerAlignement($alignement, $partie)
+    {
+        //Avoir un rendu visuel de la validation de l'alignement
+        if ($this->isChef) {
+            Alignement::validerAlignement($alignement, $partie);
+        } else {
+            return false;
+        }
+    }
+
+    /*
+     *
+     * Fonctions système
+     *
+    */
+
+    public function voirLesAlignementsProposes()
+    {
+        //Faire une indentation du texte en fonction du front
+        Alignement::getAllObjets();
+    }
+
+    /*
+     *
+     * Fonctions d'administrateur
+     *
+    */
+
+    //Expressions
+    public function ajouterExpression($textelangue, $litteraltrad, $theme, $pays, $langue)
+    {
+        //Ajouter un rendu visuel lorsque la création est finie
+        if ($this->isAdmin) {
+            Expression::ajouterExpression($textelangue, $litteraltrad, $theme, $pays, $langue);
+        } else {
+            return false;
+        }
+    }
+
+    public function modifierExpression($expression, $textelangue, $litteraltrad, $theme, $pays, $langue)
+    {
+        //Ajouter un rendu visuel lorsque la modification est finie
+        if ($this->isAdmin) {
+            Expression::modifierExpression($expression, $textelangue, $litteraltrad, $theme, $pays, $langue);
+        } else {
+            return false;
+        }
+    }
+
+    public function supprimerExpression($expression)
+    {
+        //Avoir un rendu visuel lors de la suppression
+        if ($this->isAdmin) {
+            Expression::supprimerExpression($expression);
+        } else {
+            return false;
+        }
+    }
+    //Fin expression
+
+    //Langue
+    public function ajouterLangue($langue, $code, $groupelangue)
+    {
+        //Ajouter un rendu visuel lorsque la création est finie
+        if ($this->isAdmin) {
+            Langue::ajouterLangue($langue, $code, $groupelangue);
+        }
+    }
+
+    public function modifierLangue($idlangue, $langue, $code, $groupelangue)
+    {
+        //Ajouter un rendu visuel lorsque la création est finie
+        if ($this->isAdmin) {
+            Langue::modifierLangue($idlangue, $langue, $code, $groupelangue);
+        }
+    }
+
+    public function supprimerLangue($langue)
+    {
+        //Avoir un rendu visuel lors de la suppression
+        if ($this->isAdmin) {
+            Langue::supprimerLangue($langue);
+        }
+    }
+    //Fin Langue
 }
-?>
