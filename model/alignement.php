b54f7d2fa6ec23728086b45fde758c7b7ee5f4b4
Sofiane Lasri <alasri250@gmail.com>
2023-05-21 23:00:34 +0200

    Ajout d'un require_once manquant pour le modèle Objet sur le modèle Alignement.

--- model/alignement.php
+++ model/alignement.php
@@ -1,14 +1,19 @@
 <?php
-class Alignement extends Objet {
-    
+require_once("model/objet.php");
+
+class Alignement extends Objet
+{
+
     // attributs de classe
-	protected static $objet = "Alignement";
+    protected static $objet = "Alignement";
 
     protected $id_alignement;
     protected $id_utilisateur;
 
-    public function afficher(){
+    public function afficher()
+    {
         echo Alignement::$objet . " n°" . $this->id_alignement . " est lié à l'Utilisateur n°" . $this->id_utilisateur;
     }
 }
+
 ?>
