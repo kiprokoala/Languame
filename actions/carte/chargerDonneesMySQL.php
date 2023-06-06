22ad52746cb9e69fe0fcc9e63715f88eda838be5
Farid841 <79144349+Farid841@users.noreply.github.com>
2023-06-06 22:27:27 +0200

    Update chargerDonneesMySQL.php
    Ajout de la documentation de fihcier

--- actions/carte/chargerDonneesMySQL.php
+++ actions/carte/chargerDonneesMySQL.php
@@ -1,19 +1,24 @@
 <?php
 
+// Cette ligne inclut le fichier de configuration de la connexion à la base de données
 require_once("../../conf/connexion.php");
+
+// Cette ligne établit une connexion à la base de données en utilisant la classe Connexion
 Connexion::connect();
+
+// Cette ligne inclut le fichier du modèle Pays
 require_once("../../model/paysTest.php");
 
-// 1. on récupère les tableaux de livres et d'adhérents
+// 1. on récupère les tableaux de livres et d'adhérents à partir du modèle Pays
 $pays = Pays::getAllPays();
 
-// 2. on construit le tableau de données contenant les livres et les adhérents
+// 2. on construit le tableau de données contenant les pays
 $donnees = array();
 
-// 3. on remplit ce tableau avec les deux tableaux issus des requêtes
+// 3. on ajoute le tableau des pays au tableau de données
 $donnees[] = $pays;
 
-// 4. on affiche le tableau $donnees format JSON pour qu'il soit récupéré proprement
+// 4. on affiche le tableau $donnees au format JSON pour qu'il puisse être récupéré proprement
 // par la requête AJAX à l'origine de cette recherche
 echo json_encode($donnees, JSON_UNESCAPED_UNICODE);
 ?>
